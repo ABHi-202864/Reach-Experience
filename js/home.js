11580e024615502523757ef5c903ed50b270b7aa
ABHI <abhi202864@gmail.com>
2024-05-13 21:48:43 +0530

    add hover-line reset

--- js/home.js
+++ js/home.js
@@ -1,17 +1,20 @@
-// accordion code 
-document.querySelectorAll(".accordion-item h2").forEach((accordionToggle) => {
-    accordionToggle.addEventListener("click", () => {
-        const accordionItem = accordionToggle.parentNode;
-        const accordionContent = accordionToggle.nextElementSibling;
+function accordion() {
+    document.querySelectorAll(".accordion-item h2").forEach((accordionToggle) => {
+        accordionToggle.addEventListener("click", () => {
+            const accordionItem = accordionToggle.parentNode;
+            const accordionContent = accordionToggle.nextElementSibling;
 
 
-        if (accordionContent.style.maxHeight) {
-            accordionContent.style.maxHeight = null;
-            accordionItem.classList.remove("active");
-        } else {
-            accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
-            accordionItem.classList.add("active");
-        }
+            if (accordionContent.style.maxHeight) {
+                accordionContent.style.maxHeight = null;
+                accordionItem.classList.remove("active");
+            } else {
+                accordionContent.style.maxHeight = accordionContent.scrollHeight + "px";
+                accordionItem.classList.add("active");
+            }
+        });
     });
-});
-// accordion code 
+}
+accordion();
+
+ 
